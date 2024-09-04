87e89b0ef53eddbac43bd64f4e1fa3f5d2ac2069
tobekami <brunoemelife@gmail.com>
2024-09-04 22:23:48 +0100

    Added input field to Confirmation tab

--- src/components/confirmation/confirmation.tsx
+++ src/components/confirmation/confirmation.tsx
@@ -1,10 +1,14 @@
-
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Confirmation() {
     return (
         <main>
             <div>
-                <span>Confirmation</span>
+                <div className="w-full">
+                                <Label className="font-bold text-base mb-3" htmlFor="firstname">First name</Label>
+                                <Input id="firstname" placeholder="First name" className="bg-input-bg mt-2 border border-btn-color rounded-sm focus-visible:ring-0" />
+                </div>
             </div>
         </main>
     )
